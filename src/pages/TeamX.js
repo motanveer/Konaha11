b744c9a2100fd68264f15155623c8e489f795c53
Mo <mo@motanveer.com>
2021-06-18 23:47:45 -0400

    fixed a webpack issue, i think

---
+++ src/pages/TeamX.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import * as styles from './TeamX.module.css'
+import Navbar from '../components/Navbar/Navbar'
+
+const TeamX = ()=>{
+    return(
+        <div>
+            <Navbar/>
+            <div className={styles.contentContainer}>
+                
+            </div>
+        </div>
+    )
+}
+
+export default TeamX
